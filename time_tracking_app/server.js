3f66a6fd3fa733f48f5a519d1a3dafb1c6312f4a
Josh McHugh <joshua.mchugh01@gmail.com>
2023-05-06 23:35:14 -0400

    Created Node server to handle files in ./public directory

---
+++ time_tracking_app/server.js
@@ -0,0 +1,25 @@
+'use-strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+const DATA_FILE = path.join(__dirname, 'data.json');
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', 0);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Find the server at: http://localhost:${app.get('port')}`);
+});
