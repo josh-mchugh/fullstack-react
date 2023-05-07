6341cb664beaef57f604ccae505541345efcb828
Josh McHugh <joshua.mchugh01@gmail.com>
2023-05-07 12:05:22 -0400

    Added stub for Timer component

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -47,7 +47,12 @@ class EditableTimer extends React.Component {
           );
         } else {
           return (
-            <div>Hello World!</div>
+              <Timer
+                title={this.props.title}
+                project={this.props.project}
+                elapsed={this.props.elapsed}
+                runningSince={this.props.runningSince}
+              />
           );
         }
     }
@@ -98,6 +103,38 @@ class ToggleableTimerForm extends React.Component {
                 </div>
             );
         }
+    }
+}
+
+class Timer extends React.Component {
+    render() {
+        const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+        return (
+                <div className="ui centered card">
+                  <div className="content">
+                    <div className="header">
+                      {this.props.title}
+                    </div>
+                    <div className="meta">
+                      {this.props.project}
+                    </div>
+                    <div className="center aligned description">
+                      <h2>{elapsedString}</h2>
+                    </div>
+                    <div className="extra content">
+                      <span className="right floated edit icon">
+                        <i className="edit icon" />
+                      </span>
+                      <span className="right floated trash icon">
+                        <i className="trash icon"/>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="ui button attached blue basic button">
+                    Start
+                  </div>
+                </div>
+        );
     }
 }
 
