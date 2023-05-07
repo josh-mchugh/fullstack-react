bedbc8e6345b8e2cc406d45a713bc61a68f2c632
Josh McHugh <joshua.mchugh01@gmail.com>
2023-05-07 11:00:22 -0400

    Added stub TimerForm Component

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -2,9 +2,9 @@ class TimerDashboard extends React.Component {
     render() {
         return (
             <div className="ui three column centered grid">
-                <div className="column">
-                    <EditableTimerList />
-                </div>
+              <div className="column">
+                <EditableTimerList />
+              </div>
             </div>
         );
     }
@@ -14,8 +14,20 @@ class EditableTimerList extends React.Component {
     render() {
         return (
             <div id="timers">
-                <EditableTimer />
-                <EditableTimer />
+              <EditableTimer
+                title="Learn React"
+                project="Web Domination"
+                elapsed="8986300"
+                runningSince={null}
+                editFormOpen={false}
+              />
+              <EditableTimer
+                title="Learning Extreme Ironing"
+                project="World Domincation"
+                elapsed="3890985"
+                runningSince={null}
+                editFormOpen={true}
+              />
             </div>
         );
     }
@@ -23,8 +35,47 @@ class EditableTimerList extends React.Component {
 
 class EditableTimer extends React.Component {
     render() {
+            if (this.props.editFormOpen) {
+                return (
+                    <TimerForm
+                      title={this.props.title}
+                      project={this.props.project}
+                    />
+                );
+            } else {
+                return (
+                    <div>Hello World!</div>
+                );
+            }
+    }
+}
+
+class TimerForm extends React.Component {
+    render() {
+        const submitText = this.props.title ? 'Update' : 'Create';
         return (
-            <div>Hello World!</div>
+            <div className="ui centered card">
+              <div className="content">
+                <div className="ui form">
+                  <div className="field">
+                    <label>Title</label>
+                    <input type="text" defaultValue={this.props.title} />
+                  </div>
+                  <div className="field">
+                    <label>Project</label>
+                    <input type="text" defaultValue={this.props.project} />
+                  </div>
+                  <div className="ui two button attached buttons">
+                    <button className="ui basic blue button">
+                      {submitText}
+                    </button>
+                    <button className="ui basic red button">
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
         );
     }
 }
