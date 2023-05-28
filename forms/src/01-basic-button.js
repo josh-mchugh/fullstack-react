1837e57ee647823f99668df0947af65220783499
Josh McHugh <joshua.mchugh01@gmail.com>
2023-05-28 13:56:33 -0400

    Added example of using button and it's events

--- forms/src/01-basic-button.js
+++ forms/src/01-basic-button.js
@@ -6,9 +6,30 @@ document.body.append(content);
 class Button extends React.Component {
     static displayName = '01-basic-button';
 
+    onButtonClick = (evt) => {
+        const btn = evt.target;
+        console.log(`The user clicked ${btn.name}: ${btn.value}`);
+    }
+
     render() {
         return (
-            <div>Hello World</div>
+            <div>
+              <h1>What do you think of React?</h1>
+              <button
+                name='button-1'
+                value='great'
+                onClick={this.onButtonClick}
+              >
+                Great
+              </button>
+              <button
+                name='button-2'
+                value='amazing'
+                onClick={this.onButtonClick}
+              >
+                Amazing
+              </button>
+            </div>
         );
     }
 };
