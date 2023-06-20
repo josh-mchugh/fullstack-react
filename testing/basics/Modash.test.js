1ae5469828cea3e5a09a54649dc70833afd4fbf0
Josh McHugh <joshua.mchugh01@gmail.com>
2023-06-19 21:07:29 -0400

    Added test suite for Modash

--- testing/basics/Modash.test.js
+++ testing/basics/Modash.test.js
@@ -1,3 +1,38 @@
-describe('Modash', () => {
+import Modash from './Modash';
 
+describe('Modash', () => {
+    describe('`truncate()`', () => {
+        const string = 'there was one catch, and that was CATCH-22';
+
+        it('truncates a string', () => {
+            const expected = 'there was one catch...';
+            expect(Modash.truncate(string, 19)).toEqual(expected);
+        });
+
+        it('no-ops if <= length', () => {
+            expect(Modash.truncate(string, string.length)).toEqual(string);
+        });
+    });
+
+    describe('`capitalize()`', () => {
+        it('captializes first letter, lowercase rest', () => {
+            const string = 'there was one catch, and that was CATCH-22';
+            const expected = 'There was one catch, and that was catch-22';
+            expect(Modash.capitalize(string)).toEqual(expected);
+        });
+    });
+
+    describe('`camelCase()`', () => {
+        it('camelized string with spaces', () => {
+            const string = 'customer responded at';
+            const expected = 'customerRespondedAt';
+            expect(Modash.camelCase(string)).toEqual(expected);
+        });
+
+        it('camelizes string with underscores', () => {
+            const string = 'customer_responded_at';
+            const expected = 'customerRespondedAt';
+            expect(Modash.camelCase(string)).toEqual(expected);
+        });
+    });
 });
