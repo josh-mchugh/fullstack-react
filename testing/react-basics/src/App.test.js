c233ace21a24bb1fc7ba96c8a1d2d89ffc0013b7
Josh McHugh <joshua.mchugh01@gmail.com>
2023-06-19 23:34:09 -0400

    Added behavioral driven tests with Enzyme

--- testing/react-basics/src/App.test.js
+++ testing/react-basics/src/App.test.js
@@ -25,5 +25,70 @@ describe('App', () => {
     it('should have an `input` element', () => {
         expect(wrapper.containsMatchingElement(<input/>)).toBe(true); 
     });
+
+    describe('the user populates the input', () => {
+        const item = "Vancouver";
+
+        beforeEach(() => {
+            const input = wrapper.find('input').first();
+            input.simulate('change', {
+                target: { value: item }
+            });
+        });
+
+        it('should update the state property `item`', () => {
+            expect(wrapper.state().item).toEqual(item); 
+        });
+
+        it('should enable `button`', () => {
+            const button = wrapper.find('button').first();
+            expect(button.props().disabled).toBe(false);
+        });
+
+        describe('and then clears the input', () => {
+            beforeEach(() => {
+                const input = wrapper.find('input').first();
+                input.simulate('change', {
+                    target: { value: '' } 
+                });
+            });
+
+            it('should update state property `item`', () => {
+                expect(wrapper.state().item).toEqual(''); 
+            });
+
+            it('should disable `button`', () => {
+                const button = wrapper.find('button').first();
+                expect(button.props().disabled).toBe(true);
+            });
+        });
+
+        describe('and then submits the form', () => {
+            beforeEach(() => {
+                const form = wrapper.find('form').first();
+                form.simulate('submit', {
+                    preventDefault: () => {} 
+                });
+            });
+
+            it('should add the item to state', () => {
+                expect(wrapper.state().items).toContain(item); 
+            });
+
+            it('should render the item in the table', () => {
+                expect(wrapper.containsMatchingElement(<td>{item}</td>)).toBe(true); 
+            });
+
+            it('should clear the input field', () => {
+                const input = wrapper.find('input').first();
+                expect(input.props().value).toEqual('');
+            });
+
+            it('should disable `button`', () => {
+                const button = wrapper.find('button').first();
+                expect(button.props().disabled).toBe(true);
+            });
+        });
+    });
 });
 
