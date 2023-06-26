831d2636c31781627bad99fd75dcada98976880a
Josh McHugh <joshua.mchugh01@gmail.com>
2023-06-25 23:54:27 -0400

    Setup the baseline for the application before adding routing

--- routing/basic/src/App.jsx
+++ routing/basic/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <>
+        <div className="ui text container">
+          <h2 className="ui dividing header">
+            Which body of water?
+          </h2>
+          <ul>
+            <li>
+              <a href="/atlantic">
+                <code>/atlantic</code>
+              </a>
+            </li>
+            <li>
+              <a href="/pacific">
+                <code>/pacific</code>
+              </a>
+            </li>
+          </ul>
+          <hr/>
+        </div>
+      </>
+  );
 }
 
-export default App
+const Atlantic = () => (
+    <div>
+      <h3>Atlantic Ocean</h3>
+      <p>
+        The Atlantic Ocean covers approximately 1/5th of the surface of the earth.
+      </p>
+    </div>
+);
+
+const Pacific = () => (
+    <div>
+      <h3>Pacific Ocean</h3>
+      <p>
+        Ferdinand Magellan, a Portuguese explorer, named the ocean 'mar pacifico' in 1521, which means peaceful sea.
+      </p>
+    </div>
+);
+
+export default App;
