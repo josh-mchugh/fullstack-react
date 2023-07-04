dbaebaab858caf5dee288bc0628bc2463286aed0
Josh McHugh <joshua.mchugh01@gmail.com>
2023-07-04 14:56:46 -0400

    Initial setup of the application with dummy ui and barebones redux implmentation

--- chat/src/App.jsx
+++ chat/src/App.jsx
@@ -1,11 +1,68 @@
+import React, { useState } from 'react';
+import { createStore, combineReducers } from 'redux';
+import { Provider, connect } from 'react-redux';
 import './App.css';
+
+const reducer = combineReducers({});
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <>
-      Hello World
-    </>
+      <>
+        <Provider store={store}>
+          <div className="ui container segment">
+            <Tabs />
+            <Thread />
+          </div>
+        </Provider>
+      </>
   );
+}
+
+function Tabs(props) {
+    return (
+        <div className="ui top attached tabular menu">
+          <div className="active item">
+            Title Text
+          </div>
+        </div>
+    );
+}
+
+function Thread(props) {
+    return (
+        <div className="ui center aligned basic segment">
+          <MessageList />
+          <TextFieldSubmit />
+        </div>
+    );
+}
+
+function MessageList(props) {
+    return (
+        <div className="ui comments">
+          <div className="comment">
+            <div className="text">
+              Example of message text
+              <span className="metadata">@1234567890</span>
+            </div>
+          </div>
+        </div>
+    );
+}
+
+function TextFieldSubmit(props) {
+    const [value, setValue] = useState('');
+
+    return (
+        <div className="ui input">
+          <input type="text"/>
+          <button className="ui primary button">
+            Submit
+          </button>
+        </div>
+    );
 }
 
 export default App;
